84002db3ac8ffd94e7612ea72ac5c4458b8cd292
aartisharma05 <aartisharma.official1@gmail.com>
2024-01-11 12:51:04 +0530

    bg

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,7 @@
 
 
 
-export const LOGO =
-  // "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
-"../cineinsight.png";
+export const LOGO = "../cineinsight.png";
   export const USER_AVATAR =
     "https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
 
@@ -20,7 +18,7 @@ export const IMG_CDN_URL =
   "https://image.tmdb.org/t/p/w500";
 
 
-export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/ee951142-2027-4179-ae6c-71f83929c30f/US-en-20231120-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+export const BG_URL = "../mvbg.png"
 
 export const SUPPORTED_LANGUAGES = [
   { identifier: "en", name: "English" },
